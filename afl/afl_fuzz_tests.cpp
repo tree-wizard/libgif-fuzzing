559bf748e3890abee7177186c3ed362661e46d29
Xavier Cadena <xacadena@gmail.com>
2018-12-05 21:12:36 -0600

    Update afl_fuzz_tests.cpp

--- afl/afl_fuzz_tests.cpp
+++ afl/afl_fuzz_tests.cpp
@@ -1,36 +1,14 @@
 #include "GifTranscoder.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
+#include < stdio.h > 
+#include < stdint.h > 
+#include < string.h >
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  const char * pathIn = Data;
-  const char * pathOut = "test.gif";
+int main(int argc, char * * argv) {
+  const char * pathIn = strdup(argv[1]);
+  const char * pathOut = strdup(argv[2]);
   GifTranscoder transcoder;
   int gifCode = transcoder.transcode(pathIn, pathOut);
-
   delete pathIn;
   delete pathOut;
   return 0;
 }
-
-
-#include "GifTranscoder.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  GifTranscoder transcoder;
-  //int gifCode = transcoder.transcode(Data, pathOut);
-
-  return 0;
-}
-
-
-
-look into the GIF encoding and decoding routines in gif_lib.h
-
-
-
-
